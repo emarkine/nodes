70952697134cb2f599919d7d270d3c19a3b42b47
Eugene Markine <emarkine@gmail.com>
2010-05-07 16:53:44 +0200

    ?

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,10 +1,10 @@
 class Node
   include Mongoid::Document
 
-	validates_presence_of :name
-	validates_uniqueness_of :name
-	
+  validates_presence_of :name
+  validates_uniqueness_of :name
+
   field :name
-	embedded :tags
+ # embedded :tags
 
 end
