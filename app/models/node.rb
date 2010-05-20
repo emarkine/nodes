4c5ab85594db025635e07fe962cdcfae4dda337a
Eugene Markine <emarkine@gmail.com>
2010-05-20 10:33:10 +0200

    nodes

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,22 +1,38 @@
+require "activetag/store"
+
 class Node < OrderedHash
-  include ActiveTag::Document
-
-#  validates_presence_of :name
-#  validates_uniqueness_of :name
-
-#  field :name
+	include ActiveTag::Store
 
 
-
- # embedded :tags
-
-	def save
-		  self
+	# Used for allowing accessor methods for dynamic attributes.
+	def method_missing(name, * args)
+		attr = name.to_s
+		if attr.writer?
+			# "args.size > 1" allows to simulate 1.8 behavior of "*args"
+			self[attr.reader] = (args.size > 1) ? args : args.first
+		else
+			return super unless has_key?(attr.reader)
+			self[attr.reader]
+		end
 	end
 
-	def id
-		self["_id"]
+	def [](key)
+		return super(key) if key.class != Fixnum
+		{@ordered_keys[key], self[@ordered_keys[key]]}
 	end
+
+	def []=(num, hash)
+		return super(num, hash) if num.class != Fixnum
+		@ordered_keys[num] = hash.keys[0]
+		self[hash.keys[0]] = hash.values[0]
+	end
+
+	def add(hash)
+		hash.each do |k, v|
+			self[k] = v
+		end
+	end
+
 
 
 end
---
+++ vendor/plugins/activetag/lib/activetag/store.rb
@@ -0,0 +1,82 @@
+module ActiveTag
+	module Store
+		def self.included(base)
+			base.class_eval do
+				include InstanceMethods
+				extend ClassMethods
+
+				cattr_accessor :_collection, :collection_name, :indexed
+				self.collection_name = self.name.collectionize
+				self.indexed = false
+
+				delegate :collection, :db, :to => "self.class"
+
+			end
+		end
+
+
+		module ClassMethods
+
+
+			# Return the database associated with this class.
+			def db
+				collection.db
+			end
+
+			# Returns the collection associated with this +Document+. If the
+			# document is embedded, there will be no collection associated
+			# with it.
+			#
+			# Returns: <tt>Mongo::Collection</tt>
+			def collection
+				self._collection ||= ActiveTag.config.master[self.collection_name];
+#				add_indexes
+				self._collection
+			end
+
+			alias :coll :collection
+
+			# Add the default indexes to the root document if they do not already
+			# exist. Currently this is only _type.
+			def add_indexes
+				unless indexed
+					self._collection.create_index(:_type, false)
+					self.indexed = true
+				end
+			end
+
+
+			# Returns all types to query for when using this class as the base.
+			def _types
+				@_type ||= (self.subclasses + [self.name])
+			end
+
+			
+			
+
+		end
+
+		module InstanceMethods
+
+			def id
+				 self[:_id]
+			 end
+
+			 def id=(new_id)
+				 self[:_id] = new_id
+			 end
+
+			 alias :_id :id
+			 alias :_id= :id=
+
+			def save
+				Node.coll.insert(self);
+			end
+
+	 
+
+		end
+
+
+	end
+end
