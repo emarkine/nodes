917449beb0b399555bdf90e682d3ea5cc1e28736
Eugene Markine <emarkine@gmail.com>
2010-05-20 06:05:36 +0200

    plugin running

---
+++ vendor/plugins/activetag/lib/activetag.rb
@@ -0,0 +1,29 @@
+require "rubygems"
+gem "mongo"
+
+require "mongo"
+
+require "activetag/config"
+
+
+module ActiveTag
+
+  class << self
+
+    def configure
+      config = ActiveTag::Config.instance
+      block_given? ? yield(config) : config
+    end
+
+     alias :config :configure
+  end
+
+   ActiveTag::Config.public_instance_methods(false).each do |name|
+    (class << self; self; end).class_eval <<-EOT
+      def #{name}(*args)
+        configure.send("#{name}", *args)
+      end
+    EOT
+  end
+
+end
