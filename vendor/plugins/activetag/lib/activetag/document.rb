b13e0eb1aa58293dd04df61903b6fbb8b140d55f
Eugene Markine <emarkine@gmail.com>
2010-05-20 06:08:41 +0200

    plugin running clean

--- vendor/plugins/activetag/lib/activetag/document.rb
+++ vendor/plugins/activetag/lib/activetag/document.rb
@@ -1,5 +1,26 @@
 module ActiveTag
   module Document
-    #Code here
+    def self.included(base)
+      base.class_eval do
+        include InstanceMethods
+        extend ClassMethods
+
+      end
+    end
+
+
+    module ClassMethods
+      def all
+           Array.new
+      end
+    end
+
+    module InstanceMethods
+      def name
+        "name"
+      end
+    end
+
+
   end
 end
