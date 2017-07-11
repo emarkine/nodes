20b011895326057712f142e140b5004ecdcad126
Eugene Markine <emarkine@gmail.com>
2017-07-11 18:48:22 +0200

    engine

--- lib/nodes/engine.rb
+++ lib/nodes/engine.rb
@@ -1,5 +1,11 @@
 module Nodes
   class Engine < ::Rails::Engine
     isolate_namespace Nodes
+    config.generators do |g|
+      g.test_framework :rspec, :fixture => false
+      g.fixture_replacement :factory_girl, :dir => 'spec/factories'
+      g.assets false
+      g.helper false
+    end
   end
 end
