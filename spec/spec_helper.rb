45b1567fca54fbaec9081409c672622df6f2914f
Eugene Markine <emarkine@gmail.com>
2017-07-13 23:08:49 +0200

    db

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,3 @@
-ENV['RAILS_ENV'] ||= 'test'
-
-require File.expand_path('../dummy/config/environment.rb', __FILE__)
-require 'rspec/rails'
 require 'rspec/autorun'
 require 'factory_girl_rails'
 
@@ -12,7 +8,5 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 RSpec.configure do |config|
   config.mock_with :rspec
-  config.use_transactional_fixtures = true
-  config.infer_base_class_for_anonymous_controllers = false
   config.order = 'random'
 end
