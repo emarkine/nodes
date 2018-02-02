c19b022ea3bdbf78919b2c5c67cfd1e10b9f1d70
Eugene <emarkine@gmail.com>
2018-02-02 10:34:59 +0100

    2018-02-02.10:34

---
+++ App/signal/src/main/java/com/eugenelab/nodesapp/signal/LunchActivity.java
@@ -0,0 +1,13 @@
+package com.eugenelab.nodesapp.signal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LunchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lunch);
+    }
+}
